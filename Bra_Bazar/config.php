bd0c2745d4b337271a8505eb2aa0fadc56b7cb95
Nasirmollah <mnasirmollahbd@gmail.com>
2016-09-07 00:24:33 +0600

    New Projecet added

---
+++ Bra_Bazar/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/Bra_Bazar/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/Bra_Bazar/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/Bra_Bazar/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/Bra_Bazar/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/Bra_Bazar/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/Bra_Bazar/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/Bra_Bazar/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/Bra_Bazar/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/Bra_Bazar/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/Bra_Bazar/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/Bra_Bazar/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/Bra_Bazar/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/Bra_Bazar/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'alto');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
